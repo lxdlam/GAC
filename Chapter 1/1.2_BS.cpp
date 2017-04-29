8653304fdc2a0c2726085790710c126ae6d6362c
lxdlam <lxdlam@gmail.com>
2017-04-29 18:55:58 +0800

    R

--- Chapter 1/1.2_BS.cpp
+++ Chapter 1/1.2_BS.cpp
@@ -1,24 +1,6 @@
-/*1.2节 P2 二分查找算法
-*/
-#include <vector>
+//1.2节 P2 二分查找
 
-template <typename T>
-int BinarySearch(T src[], const int &len, const T &target)
-{
-    int left = 0, right = len - 1;
-    int mid;
-    while (left <= right)
-    {
-        mid = (left + right) / 2;
-        if (src[mid] == target)
-            return mid;
-        else if (src[mid] > target)
-            right = mid - 1;
-        else
-            left = mid + 1;
-    }
-    return -1;
-}
+#include <vector>
 
 template <typename T>
 int BinarySearch(const std::vector<T> &v, const T &target)
